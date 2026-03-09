3c66af23c4014a1697a0693e3eed731d94877a54
lof310 <lof310w@gmail.com>
2026-03-08 20:13:56 -0400

    Fix

--- arch_eval/__init__.py
+++ arch_eval/__init__.py
@@ -1,9 +1,15 @@
 """arch_eval: High-level library for easy architecture evaluation of ML models."""
 
-__version__ = "0.5.0"
+__version__ = "0.1.0"
 
 from arch_eval.core.benchmark import Benchmark
-from arch_eval.core.callbacks import Callback
+from arch_eval.core.callbacks import (
+    Callback,
+    EarlyStopping,
+    LRSchedulerLogger,
+    ModelCheckpoint,
+    TensorBoardLogger,
+)
 from arch_eval.core.config import BenchmarkConfig, TrainingConfig
 from arch_eval.core.trainer import Trainer
 from arch_eval.distributed import cleanup_distributed, init_distributed
@@ -22,6 +28,10 @@ __all__ = [
     "PluginManager",
     "setup_logging",
     "Callback",
+    "EarlyStopping",
+    "ModelCheckpoint",
+    "LRSchedulerLogger",
+    "TensorBoardLogger",
     "init_distributed",
     "cleanup_distributed",
     "HyperparameterOptimizer",
